86c791f3c701fb5f0efd1ed5b91794117f4aef90
Yogesh-AGowda <124782830+Yogesh-AGowda@users.noreply.github.com>
2023-05-11 18:58:36 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,12 +1,23 @@
 import os
-# Define the directory path
-directory_path = "C:\\Users\yogesh.gowda.IMANAGE\Desktop"
-# Define the name of the folder and document
-folder_name = "Imanage_folder"
-document_name = "Imanage.txt"
-# Create the folder
-if not os.path.exists(os.path.join(directory_path, folder_name)):
-    os.makedirs(os.path.join(directory_path, folder_name))
-# Create the document and write in it
-with open(os.path.join(directory_path, folder_name, document_name), "w") as f:
-    f.write("This is an automated file created using python script and run through jenkins")
+import urllib.request
+
+# Get the AnyDesk download URL
+url = "https://download.anydesk.com/AnyDesk.exe"
+
+# Check if the AnyDesk app is already installed
+if os.path.exists("AnyDesk.exe"):
+  print("AnyDesk is already installed.")
+else:
+  # Download the AnyDesk app
+  print("Downloading AnyDesk...")
+  with urllib.request.urlopen(url) as response:
+    with open("AnyDesk.exe", "wb") as f:
+      f.write(response.read())
+
+  # Install the AnyDesk app
+  print("Installing AnyDesk...")
+  os.system("AnyDesk.exe /install")
+
+  # Start the AnyDesk app
+  print("Starting AnyDesk...")
+  os.system("AnyDesk")
